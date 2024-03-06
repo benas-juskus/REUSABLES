9da7bfd1085219be5058f0afa5944779292f9af1
KestutisVaitiekunas <kestutis.vaitiekunas@stud.ktmc.lt>
2024-03-06 10:05:12 +0200

    added interface for user

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -1,22 +1,34 @@
 // import { PrismaClient } from '@prisma/client';
 const { PrismaClient } = require('@prisma/client');
 
+interface User {
+    username?: string;
+    email?: string;
+    password?: string;
+    role_id?: number;
+}
 const prisma = new PrismaClient();
 
 const User = {
 
-    create: async (userdata) => {
+    create: async (userdata: User) => {
         console.log("userdata", userdata);
         const user = await prisma.users.create({
             data: {
                 username: userdata.username,
                 email: userdata.email,
                 password: userdata.password,
-                // role: {connect: {id: userdata.role_id}}, 
-                roleId: userdata.role_id
+                role_id: userdata.role_id
             }
         })
     },
+    showUser: async (userdata: User) => {
+        const user = await prisma.users.findUnique({
+            data: {
+                email: userdata.email
+            }
+        })
+    }
     
 }
 
