928d6e23c6eb4ad18fcaca9c9255adfa1bfce4d5
Pulveraizor <benas.juskus@gmail.com>
2024-03-12 16:48:32 +0200

    Created User details section layout for dashboard

--- client/src/components/pages/dashboard/dashboard.jsx
+++ client/src/components/pages/dashboard/dashboard.jsx
@@ -2,13 +2,39 @@ import Heeader from '../../../components/layout/header';
 import Footer from '../../../components/layout/footer';
 import {Container} from "@mui/material";
 import colorTheme from '../../layout/colorTheme';
+import UserDetails from './dashboardComponents/userDetails';
+
+const user_data = {
+    id: 3,
+    rating: 3.3,
+    username: "Meghan",
+    photo: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2F1.bp.blogspot.com%2F-TlIRtOXmmDA%2FWS8Ar-jMLDI%2FAAAAAAAAH58%2Fb03hdJmdDTs6j0X9d9FQOlHWAHYTJC6KQCK4B%2Fs1600%2FStock%252Bimages%252Bare%252Boften%252Bcasted%252Bwith%252Battractive%252Bpeople%252B...-790465.jpg"
+};
+
+const ItemsList = () => {
+    return (
+        <>
+
+        </>
+    )
+};
+
+const RecentChats = () => {
+    return (
+        <>
+
+        </>
+    )
+}
 
 const Dashboard = () => {
     return (
         <>
-            <Heeader />
+            <Heeader data={user_data}/>
                 <Container style={{backgroundColor: colorTheme.palette.secondary.light}}>
-                I am the container
+                    <UserDetails user_data={user_data}/>
+                    <ItemsList></ItemsList>
+                    <RecentChats></RecentChats>
                 </Container>
             <Footer />
         </>
---
+++ client/src/components/pages/dashboard/dashboardComponents/userDetails.jsx
@@ -0,0 +1,79 @@
+import Rating from '@mui/material/Rating';
+import { Button} from "@mui/material";
+import colorTheme from '../../../layout/colorTheme';
+
+const RatingDisplay = ({value}) => {
+    return (
+        <div style={{display: "flex", flexDirection: "column", alignItems: "center", marginTop: "3em"}}>
+            <span>Your current rating is</span>
+            <h1 style={{margin: "0"}}>{value}</h1>  
+            <Rating name="user_rating" value={value} readOnly precision={0.1} size="large"/>
+        </div>
+    )
+}
+
+const ProfilePicture = ({image}) => {
+    return (
+        <img 
+            alt="profile_image" 
+            src={image}
+            style={{
+                width: "20em", 
+                height: "20em", 
+                borderRadius: "50%",
+                objectFit: "cover",
+                }}>
+        </img>
+    )
+};
+
+const toggleUsernameChange = () => {
+    // TODO
+}
+
+const UsernameControlBtn = ({username}) => {
+    return (
+        <Button 
+            variant="contained" 
+            onClick={toggleUsernameChange}
+            style={{
+                height: "3em",
+                marginTop: "0.8em",
+                backgroundColor: colorTheme.palette.primary.dark
+            }}>Change Username
+        </Button>
+    )
+};
+
+const ChangePasswordBtn = () => {
+    return (
+        <Button 
+            variant="contained" 
+            style={{
+                height: "3em", 
+                marginTop: "0.8em", 
+                color: "black", 
+                backgroundColor: colorTheme.palette.secondary.light
+                }}>
+        Change Password
+        </Button>
+    )
+}
+
+const UserDetails = ({user_data}) => {
+    return (
+        <div style={{
+                display: "flex", 
+                flexDirection: "column", 
+                alignItems: "center", 
+                marginTop: "3em"}}> 
+            <RatingDisplay value={user_data.rating}/>
+            <ProfilePicture image={user_data.photo}></ProfilePicture>
+            <h2>{user_data.username}</h2>
+            <UsernameControlBtn username={user_data.username}></UsernameControlBtn>
+            <ChangePasswordBtn></ChangePasswordBtn>
+        </div>
+    )
+};
+
+export default UserDetails;
