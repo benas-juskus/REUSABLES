e50c8c85c3f0dac138c291486892e015b8c317d4
Vilius Užkurėlis <vilius.uzkurelis@stud.ktmc.lt>
2024-03-12 09:57:53 +0200

    control_panel

--- server/routes/SubCategoriesRoute.ts
+++ server/routes/SubCategoriesRoute.ts
@@ -5,15 +5,8 @@ const router = express.Router();
 router.get("/first", SubCategoriesController.getSubCategory);
 router.get("/all", SubCategoriesController.getAllSubCategories);
 router.get("/:id", SubCategoriesController.getSubCategoryById);
-router.post("/", SubCategoriesController.createSubCategory, (req, res, next) => {
-    console.log("Subcategory created successfully");
-    next(); // Call next middleware or send response
-  });
-  
-router.patch("/:id", SubCategoriesController.updateSubCategory, (req, res, next) => {
-  console.log("received");
-  next(); // Call next middleware or send response
-});
+router.post("/", SubCategoriesController.createSubCategory, (req, res, next) => {});
+router.patch("/:id", SubCategoriesController.updateSubCategory, (req, res, next) => {});
 router.delete("/:id", SubCategoriesController.deleteSubCategory);
 
 module.exports = router;
